bff2782ac6a6bfa4a6a410f5f5d92b31aa21c53d
Kostiantyn Rybnikov <k-bx@k-bx.com>
2017-01-04 01:37:40 +0200

    Move things around

--- th-utils-intero-test/src/Main.hs
+++ th-utils-intero-test/src/Main.hs
@@ -1,16 +1,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Main where
+module Main
+  ( main
+  ) where
 
 import Data.String.Class (toString)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.Hamlet
-import Text.Julius
-import Text.Shakespeare
-import Utils
 import qualified Data.Text.Lazy as TL
 
 main :: IO ()
 main = do
-  let html = $(shamletFileRelative "data/foo.hamlet")
+  let html = $(shamletFile "data/foo.hamlet")
   putStrLn (toString (renderHtml html :: TL.Text))
